0fa8d46fa1663f466db0af47093adb8d3604cd45
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2022-11-18 23:06:05 +0000

    Creando una Migración

--- TareasMVC/Program.cs
+++ TareasMVC/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>(opciones => opciones.UseSqlServer("name=DeafultConnection"));
+builder.Services.AddDbContext<ApplicationDbContext>(opciones => opciones.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
