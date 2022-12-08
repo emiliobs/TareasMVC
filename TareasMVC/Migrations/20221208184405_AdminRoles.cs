75d1c3ac32e23d7eebe49c3470d771efae20132f
emilio barrera <barrera_emilio@yahoo.es>
2022-12-08 18:47:14 +0000

    Roles

---
+++ TareasMVC/Migrations/20221208184405_AdminRoles.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TareasMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"IF NOT Exists(Select Id From AspNetRoles where Id = '1d5a17a7-5cad-4222-a0d5-5a5a8899cea9')
+                                    Begin
+	                                    Insert AspNetRoles (Id, Name, NormalizedName)
+                                        Values ('1d5a17a7-5cad-4222-a0d5-5a5a8899cea9', 'admin','ADMIN')
+                                    End
+                                ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"Delete AspNetRoles Where Id = '1d5a17a7-5cad-4222-a0d5-5a5a8899cea9'");
+        }
+    }
+}
