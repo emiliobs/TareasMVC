f9c5c55b3c1ebc674d7e67b540602a85a731a9ef
emilio barrera <barrera_emilio@yahoo.es>
2023-01-01 02:04:07 +0000

    Gregando una NUeva Tarea en Memoria

--- TareasMVC/wwwroot/js/tareas.js
+++ TareasMVC/wwwroot/js/tareas.js
@@ -1,6 +1,10 @@
 ﻿function agregarNuevaTareaAlListado() {
 
-    tareasListadoViewModel.tareas.push(new tareasElementoListadoViewModel({id: 0, titulo: ''}));
+    tareasListadoViewModel.tareas.push(new tareasElementoListadoViewModel({ id: 0, titulo: '' }));
+
+    //jquery
+    $("[name=titulo-tarea]").last().focus();
+
 }
 
 function manejadorFocusoutTituloTarea(tarea) {
